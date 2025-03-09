9b6c19492c25c83b35bf3b5ee450d7d5fcda84b7
Rory-Miley <rorypmiley@gmail.com>
2025-03-09 01:29:05 -0500

    added core functionality
    made the bones of the price finder system, still in terminal

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 Used to record receipts and automate their organisation
 
+## What are the reqs?
+    pip: 
+        - Pillow
+        - pillow_heic
+        - os
+        - pytesseract
+
+    homebrew:
+        - tesseract
---
+++ src/Python/main.py
@@ -0,0 +1,61 @@
+from PIL import Image
+import pillow_heif as ph
+import os
+import pytesseract
+import re
+
+# Set Tesseract path (adjust based on your installation)
+# pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"  # Homebrew default path
+
+
+## FUNTIONS-----------------------------------##
+def convert_heic_to_jpg(image_path, output_path):
+    heif_img = ph.open_heif(image_path)
+    image = Image.frombytes(heif_img.mode, heif_img.size, heif_img.data)
+    image.save(output_path, 'JPEG')
+
+def check_image_format(image_path):
+    if image_path.endswith('.heic') or image_path.endswith('.HEIC'):
+        return 'heic'
+    elif image_path.endswith('.jpg'):
+        return 'jpg'
+    else:
+        return 'unsupported'
+
+def process_image(image_path):
+    # Open the image
+    image = Image.open(image_path)
+    # Perform OCR on the image
+    return pytesseract.image_to_string(image)
+
+def extract_price(text):
+    price_pattern = r'(?i)(?:\$|USD\s?)?\d{1,5}(?:[.,]\d{2})?'
+    prices = re.findall(price_pattern, text)
+    
+    # Return the first valid price (if any)
+    return prices[0] if prices else None
+
+## MAIN----------------------------------------##
+# Load an image from user prompt
+ans = input('Do you want to load test image? (y/n): ')
+if ans == 'y' or ans == 'Y':
+    image_path = os.path.expanduser('~/Github/receipt-me/src/Python/test_images/IMG_0014.HEIC')
+else:
+    image_path = input('Type the path of the image: ')
+
+# Check the image format
+if check_image_format(image_path) == 'heic':
+    convert_heic_to_jpg(image_path, 'temp.jpg')
+    image_path = 'temp.jpg'
+elif check_image_format(image_path) == 'unsupported':
+    print('Unsupported format...')
+    exit()
+
+extracted_text = process_image(image_path)
+extracted_price = extract_price(extracted_text)
+
+# Print the extracted text
+print("\n[EXTRACTED TEXT]")
+print(extracted_text)
+print("\n[EXTRACTED PRICE]")
+print(extracted_price)
